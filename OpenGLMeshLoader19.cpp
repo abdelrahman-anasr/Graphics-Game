0eab4315916a329885769d8b229de384a4de38d1
lojy97 <lojine.elhwary@gmail.com>
2024-12-25 19:58:09 +0200

    revert

--- OpenGLMeshLoader19.cpp
+++ OpenGLMeshLoader19.cpp
@@ -12,6 +12,8 @@
 #include <chrono>
 #include <glut.h>
 
+
+//Testing Change as Abdulrahman
 #define GLUT_KEY_ESCAPE 27
 
 #define M_PI 3.14159265358979323846
@@ -19,20 +21,6 @@ float playerx = 0.0f;
 float playery = 0.0f;
 float playerz = 0.0f;
 float playerrotation = 0.0f;
-
-#include <vector>
-#include <chrono>
-
-
-// Timer variables
-float elapsedTime = 0.0f;
-float deltaTime;
-
-struct Point3D {
-    float x, y, z;
-};
-
-
 
 class Vector3f {
 public:
@@ -86,37 +74,28 @@ void calculateDeltaTime() {
 }
 
 
-//Player position
-double playerx = 0.0;
-double playery = 5.0; // Player height (important)
-double playerz = 0.0;
-double playerRotation = 0.0;
 
-
-// Camera class
 class Camera {
 public:
-    Vector3f eye, center, up, cameraFront;
+    Vector3f eye, center, up;
 
     Camera() {
-        eye = Vector3f(0.0f, 0.0f, 3.0f);
-        center = Vector3f(0.0f, 0.0f, 0.0f);
+        eye = Vector3f(0.0f, 1.8f, 5.0f);
+        center = Vector3f(0.0f, 1.8f, 0.0f);
         up = Vector3f(0.0f, 1.0f, 0.0f);
-        cameraFront = Vector3f(0.0f, 0.0f, -1.0f);
     }
 
-    // Sets the camera view using gluLookAt
     void look() {
         gluLookAt(
             eye.x, eye.y, eye.z,
-            eye.x + cameraFront.x, eye.y + cameraFront.y, eye.z + cameraFront.z,
+            center.x, center.y, center.z,
             up.x, up.y, up.z
         );
     }
 };
 
-// Intialize camera + Model + Textures
 Camera camera;
+
 GLuint tex_skybox;
 GLTexture tex_ground;
 Model_3DS model_player;
@@ -125,38 +104,12 @@ float radians(float degrees) {
     return degrees * M_PI / 180.0f;
 }
 
-// Variables for camera rotation
-float yaw = -90.0f;
+float yaw = -90.0f;   
 float pitch = 0.0f;
-float lastX = 640, lastY = 360;
+float lastX = 640, lastY = 360; 
 bool firstMouse = true;
-Vector3f cameraFront;
+Vector3f cameraFront; 
 
-
-// Helper Functions
-
-// Calculate delta time for smooth frame rate
-void calculateDeltaTime() {
-    static auto startTime = std::chrono::high_resolution_clock::now();
-    auto now = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> duration = now - startTime;
-    startTime = now;
-    deltaTime = duration.count();
-}
-
-// Calculate angle between two points
-float calculateAngle(float x1, float y1, float z1, float x2, float y2, float z2) {
-    float dx = x2 - x1;
-    float dy = y2 - y1;
-    float dz = z2 - z1;
-    return atan2(dy, sqrt(dx * dx + dz * dz)) * (180.0f / M_PI);
-}
-
-
-
-// Setups Functions
-
-// Setup lights
 void setupLights() {
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
@@ -170,7 +123,6 @@ void setupLights() {
     glLightfv(GL_LIGHT0, GL_POSITION, position);
 }
 
-<<<<<<< HEAD
 class Bullet {
 public:
     float dmg;
@@ -277,9 +229,6 @@ void updateBullets(){
 }
 
 
-=======
-// Setup camera
->>>>>>> 0e184758dd8ce03e9ae0579e1835e24164e8ea27
 void setupCamera() {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -290,37 +239,49 @@ void setupCamera() {
     camera.look();
 }
 
+void updateCamera() {
+    cameraFront.x = cos(radians(yaw)) * cos(radians(pitch));
+    cameraFront.y = sin(radians(pitch));
+    cameraFront.z = sin(radians(yaw)) * cos(radians(pitch));
+    cameraFront = cameraFront.unit();
 
+    float distanceFromPlayer = 5.0f; 
+    camera.eye.x = playerx - cameraFront.x * distanceFromPlayer;
+    camera.eye.y = playery + 2.0f; 
+    camera.eye.z = playerz - cameraFront.z * distanceFromPlayer;
 
-// Draw Functions
+    camera.center.x = playerx;
+    camera.center.y = playery + 1.0f;
+    camera.center.z = playerz;
 
-// Draw ground
+    camera.up = Vector3f(0.0f, 1.0f, 0.0f);
+}
+
 void drawGround() {
     glEnable(GL_LIGHTING);
 
     glColor3f(1, 1, 1);
-    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_TEXTURE_2D);  
 
-    glBindTexture(GL_TEXTURE_2D, tex_ground.texture[0]);
+    glBindTexture(GL_TEXTURE_2D, tex_ground.texture[0]); 
 
     glPushMatrix();
     glBegin(GL_QUADS);
-    glNormal3f(0, 1, 0);
+    glNormal3f(0, 1, 0); 
     glTexCoord2f(0, 0);
-    glVertex3f(-500, 0, -500);
+    glVertex3f(-50, 0, -50);
     glTexCoord2f(10, 0);
-    glVertex3f(500, 0, -500);
+    glVertex3f(50, 0, -50);
     glTexCoord2f(10, 10);
-    glVertex3f(500, 0, 500);
+    glVertex3f(50, 0, 50);
     glTexCoord2f(0, 10);
-    glVertex3f(-500, 0, 500);
+    glVertex3f(-50, 0, 50);
     glEnd();
     glPopMatrix();
 
     glDisable(GL_TEXTURE_2D);
 }
 
-// Draw skybox
 void drawSkybox() {
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, tex_skybox);
@@ -337,24 +298,26 @@ void drawSkybox() {
     glDisable(GL_TEXTURE_2D);
 }
 
+void drawModel() {
+    glPushMatrix();
+    glTranslated(playerx, playery, playerz);
+    glRotated(-yaw + 90.0f, 0, 1, 0);
+    glRotated( 90.0f, 1,0, 0); 
 
+    glScaled(0.025, 0.025, 0.025);
+    model_player.Draw();
+    glPopMatrix();
+}
 
-<<<<<<< HEAD
 weapon cw(true);
 
 void Display() {
     calculateDeltaTime();
     setupCamera();
     setupLights();
-=======
-// Player Movement
->>>>>>> 0e184758dd8ce03e9ae0579e1835e24164e8ea27
 
-// Movement speed + sensitivity
-const float moveSpeed = 0.1f;
-const float sensitivity = 0.05f;
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-<<<<<<< HEAD
     Vector3f pos(playerx, playery, playerz);
     cw.position = pos;
     drawGround();
@@ -379,14 +342,8 @@ void Anim() {
     
     glutPostRedisplay();
 }
-=======
-// Keyboard input
-bool keyStates[256] = { false };
->>>>>>> 0e184758dd8ce03e9ae0579e1835e24164e8ea27
 
-// Keyboard input for movement
 void Keyboard(unsigned char key, int x, int y) {
-<<<<<<< HEAD
     float moveSpeed = 0.2f;
     Vector3f front(cameraFront.x, 0.0f, cameraFront.z);
     front = front.unit();
@@ -413,26 +370,14 @@ void Keyboard(unsigned char key, int x, int y) {
         shoot(cw);
         break;
     case GLUT_KEY_ESCAPE:
-=======
-    keyStates[key] = true;
-    if (key == GLUT_KEY_ESCAPE) {
->>>>>>> 0e184758dd8ce03e9ae0579e1835e24164e8ea27
         exit(0);
+        break;
     }
+    updateCamera();
+    glutPostRedisplay();
 }
 
-// Keyboard hold input for smooth movement
-void KeyboardUp(unsigned char key, int x, int y) {
-    keyStates[key] = false;
-}
-
-// Mouse input for camera rotation
 void Motion(int x, int y) {
-    int windowWidth = 1280;
-    int windowHeight = 720;
-    int centerX = windowWidth / 2;
-    int centerY = windowHeight / 2;
-
     if (firstMouse) {
         lastX = x;
         lastY = y;
@@ -440,127 +385,32 @@ void Motion(int x, int y) {
     }
 
     float xoffset = x - lastX;
-    float yoffset = lastY - y;
+    float yoffset = lastY - y; 
     lastX = x;
     lastY = y;
 
+    float sensitivity = 0.3f; 
     xoffset *= sensitivity;
     yoffset *= sensitivity;
 
     yaw += xoffset;
     pitch += yoffset;
 
-    if (pitch > 89.0f)
-        pitch = 89.0f;
-    if (pitch < -89.0f)
-        pitch = -89.0f;
+    if (pitch > 45.0f)
+        pitch = 45.0f;
+    if (pitch < -45.0f)
+        pitch = -45.0f;
 
-    camera.cameraFront.x = cos(radians(yaw)) * cos(radians(pitch));
-    camera.cameraFront.y = sin(radians(pitch));
-    camera.cameraFront.z = sin(radians(yaw)) * cos(radians(pitch));
-    camera.cameraFront = camera.cameraFront.unit();
-
-    camera.look();
+    updateCamera();
     glutPostRedisplay();
-
-    glutWarpPointer(centerX, centerY);
-    lastX = centerX;
-    lastY = centerY;
 }
 
-// Update player position
-void updatePlayerPosition() {
-    Vector3f front(camera.cameraFront.x, 0.0f, camera.cameraFront.z);
-    front = front.unit();
-    Vector3f right = front.cross(camera.up).unit();
-
-    if (keyStates['w']) {
-        playerx += front.x * moveSpeed;
-        playerz += front.z * moveSpeed;
-    }
-    if (keyStates['s']) {
-        playerx -= front.x * moveSpeed;
-        playerz -= front.z * moveSpeed;
-    }
-    if (keyStates['a']) {
-        playerx -= right.x * moveSpeed;
-        playerz -= right.z * moveSpeed;
-    }
-    if (keyStates['d']) {
-        playerx += right.x * moveSpeed;
-        playerz += right.z * moveSpeed;
-    }
-
-    camera.eye.x = playerx;
-    camera.eye.y = playery + 1.8f;
-    camera.eye.z = playerz;
-
-    camera.look();
-}
-
-// Update camera
-void updateCamera() {
-
-    cameraFront.x = cos(radians(yaw)) * cos(radians(pitch));
-    cameraFront.y = sin(radians(pitch));
-    cameraFront.z = sin(radians(yaw)) * cos(radians(pitch));
-    cameraFront = cameraFront.unit();
-
-    float distanceFromPlayer = 5.0f;
-    camera.eye.x = playerx - cameraFront.x * distanceFromPlayer;
-    camera.eye.y = playery + 5.0f;
-    camera.eye.z = playerz - cameraFront.z * distanceFromPlayer;
-
-    camera.center.x = playerx;
-    camera.center.y = playery + 5.0f;
-    camera.center.z = playerz;
-
-    camera.up = Vector3f(0.0f, 5.0f, 0.0f);
-}
-
-
-
-// Game Functions (loading/intializations + update/Idle)
-
-// Load textures + models
 void LoadAssets() {
     model_player.Load("Models/Male1_3ds/Male1.3ds");
     tex_ground.Load("Textures/ground.bmp");
-    loadBMP(&tex_skybox, "Textures/moon.bmp", true);
+    loadBMP(&tex_skybox, "Textures/sky.bmp", true);
 }
 
-// Update function (player movement)
-void update(float deltaTime) {
-    updatePlayerPosition();
-
-    updateCamera();
-}
-
-// Idle function (calculate deltatime + update)
-void idle() {
-    calculateDeltaTime();
-    update(deltaTime);
-
-    glutPostRedisplay();
-}
-
-// Display function (draw everything + setup lights/camera)
-void Display() {
-    float deltaTime = 0.016f;
-    updatePlayerPosition();
-
-    setupCamera();
-    setupLights();
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    drawGround();
-    drawSkybox();
-
-    glutSwapBuffers();
-}
-
-// Initialize OpenGL settings
 void Init() {
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
@@ -574,7 +424,6 @@ void Init() {
     glClearColor(0.5f, 0.7f, 1.0f, 1.0f);
 
     glutSetCursor(GLUT_CURSOR_NONE);
-
 }
 void time(int val) {
     int index = 0;
@@ -587,13 +436,8 @@ void time(int val) {
 
 
 
-<<<<<<< HEAD
 void main(int argc, char** argv) {
 
-=======
-// Main function
-int main(int argc, char** argv) {
->>>>>>> 0e184758dd8ce03e9ae0579e1835e24164e8ea27
     glutInit(&argc, argv);
 
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
@@ -601,12 +445,11 @@ int main(int argc, char** argv) {
     glutInitWindowSize(1280, 720);
     glutInitWindowPosition(100, 100);
 
-    glutCreateWindow("First Person Shooter");
+    glutCreateWindow("Escape The Island");
 
     glutDisplayFunc(Display);
-    glutIdleFunc(idle);
+    glutIdleFunc(Display);
     glutKeyboardFunc(Keyboard);
-    glutKeyboardUpFunc(KeyboardUp);
     glutPassiveMotionFunc(Motion);
 
     LoadAssets();
@@ -615,6 +458,4 @@ int main(int argc, char** argv) {
     glutTimerFunc(0, time, 0);
 
     glutMainLoop();
-
-    return 0;
 }
